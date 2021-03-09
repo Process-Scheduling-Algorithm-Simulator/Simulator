162933064791d90fd0c5c4162cf9763d0a870950
Neeraj-Mirji <61899469+Neeraj-Mirji@users.noreply.github.com>
2021-03-09 08:56:42 +0530

    Add files via upload

--- mainWindow.py
+++ mainWindow.py
@@ -1,10 +1,15 @@
 
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from simulatorWindow import*
 
 
 class Ui_MainWindow(object):
+
+    def __init__(self):
+        super().__init__()
+        self.n = 0                                         # n for number of processes
+        self.option = 0                                    # option for selection of algo
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -21,6 +26,9 @@ class Ui_MainWindow(object):
         self.backgroundImage.setPixmap(QtGui.QPixmap("images/main_background2.jpg"))
         self.backgroundImage.setScaledContents(True)
         self.backgroundImage.setObjectName("backgroundImage")
+
+        #for label1
+
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(290, 40, 471, 101))
         font = QtGui.QFont()
@@ -35,6 +43,9 @@ class Ui_MainWindow(object):
 "font-size : 42px;\n"
 "}")
         self.label.setObjectName("label")
+
+        #for label2
+
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(450, 120, 411, 41))
         font = QtGui.QFont()
@@ -50,6 +61,9 @@ class Ui_MainWindow(object):
 "color : rgb(255, 85, 6);\n"
 "}")
         self.label_2.setObjectName("label_2")
+
+        #frame 
+        
         self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setGeometry(QtCore.QRect(350, 220, 411, 221))
         self.frame.setStyleSheet("QFrame\n"
@@ -61,6 +75,9 @@ class Ui_MainWindow(object):
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+
+        #label3
+
         self.label_3 = QtWidgets.QLabel(self.frame)
         self.label_3.setGeometry(QtCore.QRect(20, 50, 171, 51))
         self.label_3.setStyleSheet("QLabel\n"
@@ -70,6 +87,9 @@ class Ui_MainWindow(object):
 "font-size : 22px;\n"
 "}")
         self.label_3.setObjectName("label_3")
+        
+         #label3
+
         self.label_4 = QtWidgets.QLabel(self.frame)
         self.label_4.setGeometry(QtCore.QRect(20, 120, 161, 41))
         self.label_4.setStyleSheet("QLabel\n"
@@ -79,15 +99,9 @@ class Ui_MainWindow(object):
 "font-size : 22px;\n"
 "}")
         self.label_4.setObjectName("label_4")
-        self.textEdit = QtWidgets.QTextEdit(self.frame)
-        self.textEdit.setGeometry(QtCore.QRect(220, 60, 161, 41))
-        self.textEdit.setStyleSheet("QTextEdit\n"
-"{\n"
-"border:none;\n"
-"border-radius:2px;\n"
-"font-size:22px;\n"
-"}")
-        self.textEdit.setObjectName("textEdit")
+        
+        #comboBox
+
         self.comboBox = QtWidgets.QComboBox(self.frame)
         self.comboBox.setGeometry(QtCore.QRect(220, 120, 161, 41))
         self.comboBox.setStyleSheet("QComboBox\n"
@@ -105,6 +119,12 @@ class Ui_MainWindow(object):
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
+
+        #lineEdit
+
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setGeometry(QtCore.QRect(220, 60, 161, 41))
+        self.lineEdit.setObjectName("lineEdit")
         self.start = QtWidgets.QPushButton(self.centralwidget)
         self.start.setGeometry(QtCore.QRect(500, 460, 131, 51))
         font = QtGui.QFont()
@@ -123,6 +143,9 @@ class Ui_MainWindow(object):
 "  font-size: 24px;\n"
 "}\n"
 "")
+        self.onlyInt = QtGui.QIntValidator()
+        self.lineEdit.setValidator(self.onlyInt)              #validations for number of processes
+
         self.start.setObjectName("start")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -132,6 +155,9 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.start.clicked.connect(self.getNum)                 #connecting to getN  
+        self.start.clicked.connect(self.getAlgo)                #connecting to getAlgo
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Simulator"))
@@ -139,15 +165,40 @@ class Ui_MainWindow(object):
         self.label_2.setText(_translate("MainWindow", "SIMULATION PROGRAM"))
         self.label_3.setText(_translate("MainWindow", "No of Process   : "))
         self.label_4.setText(_translate("MainWindow", "Algorithm         :"))
-        self.textEdit.setStatusTip(_translate("MainWindow", "  Input the number of process"))
         self.comboBox.setStatusTip(_translate("MainWindow", "  Select the Scheduling Algorithm"))
         self.comboBox.setItemText(1, _translate("MainWindow", "FCFS"))
         self.comboBox.setItemText(2, _translate("MainWindow", "RR"))
         self.comboBox.setItemText(3, _translate("MainWindow", "Priority-np"))
-        self.comboBox.setItemText(4, _translate("MainWindow", "Priorit-p"))
+        self.comboBox.setItemText(4, _translate("MainWindow", "Priority-p"))
         self.comboBox.setItemText(5, _translate("MainWindow", "SJF"))
         self.comboBox.setItemText(6, _translate("MainWindow", "SRTF"))
         self.start.setText(_translate("MainWindow", "Start"))
+
+    def getNum(self):
+        self.n = int(self.lineEdit.text())
+        print(self.n)
+        
+        
+    def getAlgo(self):
+        txt = self.comboBox.currentText()
+        print(txt)
+        if txt == "FCFS": self.option = 0
+        if txt == "RR": self.option = 1
+        if txt == "Priority-np": self.option = 2
+        if txt == "Priority-p": self.option = 3
+        if txt == "SJF": self.option = 4
+        if txt == "SRTF": self.option = 5
+        print(self.option)
+        self.openSimulator()
+
+    def openSimulator(self):                                  #calling the second window
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow1()
+        self.ui.n = self.n                                    # setting the n and option variables of simulator window
+        self.ui.option = self.option
+        self.ui.setupUi(self.window)
+        self.window.show()
+        
 
 
 if __name__ == "__main__":
