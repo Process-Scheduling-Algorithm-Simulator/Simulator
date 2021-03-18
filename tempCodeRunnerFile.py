b646c961b41e147f323a63a64e0bcd9b2b058d74
Neeraj-Mirji <61899469+Neeraj-Mirji@users.noreply.github.com>
2021-03-18 19:57:49 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-        # for i in range(size):
-        #     index = startTime[i][0]
-        #     start = startTime[i][1]
-        #     duration = length[i][1]
-        #     gnt.broken_barh([(start, duration)], (10, 10), facecolors =(colors[index]))
-           
-        # self.canvas.draw()
-        # gnt.legend(processName) 
